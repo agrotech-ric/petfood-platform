049bca614f84f9f94772e1b4daf7278b4b0d85b6
agrotech-ric <agrotech.alert@mail.ru>
2026-05-05 17:37:38 +0500

    from User to USER

--- backend-main/services/account/src/main/java/dev/pet/account/api/InternalUsersController.java
+++ backend-main/services/account/src/main/java/dev/pet/account/api/InternalUsersController.java
@@ -21,7 +21,7 @@ public class InternalUsersController {
     }
 
     @GetMapping("/{id}/email")
-    @PreAuthorize("hasRole('VET') or hasRole('ADMIN') or hasRole('User')")
+    @PreAuthorize("hasRole('VET') or hasRole('ADMIN') or hasRole('USER')")
     public InternalUserEmailResponse getEmailById(@PathVariable UUID id) {
         User u = users.findById(id)
             .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "User not found"));
