fc5855d51d21768e3f6fd1c0a65a0af30238bcde
agrotech-ric <agrotech.alert@mail.ru>
2026-07-31 13:36:36 +0500

    fix recommendation disease flow

--- frontend-main/src/components/NutrientRanges.tsx
+++ frontend-main/src/components/NutrientRanges.tsx
@@ -14,31 +14,31 @@ export const NutrientRanges = ({ nutrientRanges, onUpdateRange }: NutrientRanges
       <div className={styles.rangeSliders}>
         <DualRangeSlider
           label="Влага:"
-          min={nutrientRanges.moisture.min}
-          max={nutrientRanges.moisture.max}
-          onMinChange={(val) => onUpdateRange('moisture', 'min', val)}
-          onMaxChange={(val) => onUpdateRange('moisture', 'max', val)}
+          min={nutrientRanges.moisture_per.min}
+          max={nutrientRanges.moisture_per.max}
+          onMinChange={(val) => onUpdateRange('moisture_per', 'min', val)}
+          onMaxChange={(val) => onUpdateRange('moisture_per', 'max', val)}
         />
         <DualRangeSlider
           label="Белки:"
-          min={nutrientRanges.protein.min}
-          max={nutrientRanges.protein.max}
-          onMinChange={(val) => onUpdateRange('protein', 'min', val)}
-          onMaxChange={(val) => onUpdateRange('protein', 'max', val)}
+          min={nutrientRanges.protein_per.min}
+          max={nutrientRanges.protein_per.max}
+          onMinChange={(val) => onUpdateRange('protein_per', 'min', val)}
+          onMaxChange={(val) => onUpdateRange('protein_per', 'max', val)}
         />
         <DualRangeSlider
           label="Углеводы:"
-          min={nutrientRanges.carbs.min}
-          max={nutrientRanges.carbs.max}
-          onMinChange={(val) => onUpdateRange('carbs', 'min', val)}
-          onMaxChange={(val) => onUpdateRange('carbs', 'max', val)}
+          min={nutrientRanges.carbohydrate_per.min}
+          max={nutrientRanges.carbohydrate_per.max}
+          onMinChange={(val) => onUpdateRange('carbohydrate_per', 'min', val)}
+          onMaxChange={(val) => onUpdateRange('carbohydrate_per', 'max', val)}
         />
         <DualRangeSlider
           label="Жиры:"
-          min={nutrientRanges.fats.min}
-          max={nutrientRanges.fats.max}
-          onMinChange={(val) => onUpdateRange('fats', 'min', val)}
-          onMaxChange={(val) => onUpdateRange('fats', 'max', val)}
+          min={nutrientRanges.fats_per.min}
+          max={nutrientRanges.fats_per.max}
+          onMinChange={(val) => onUpdateRange('fats_per', 'min', val)}
+          onMaxChange={(val) => onUpdateRange('fats_per', 'max', val)}
         />
       </div>
     </>
